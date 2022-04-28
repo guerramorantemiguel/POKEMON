060cbfd87e76d1107cf8f709e6133aa9609a8776
miguelguerra <100090620+guerramorantemiguel@users.noreply.github.com>
2022-04-28 07:12:18 +0000

    commit

--- Ejercicio.py
+++ Ejercicio.py
@@ -1,3 +1,6 @@
 import pandas as pd
-notas = pd.read_csv("poke_mon.csv", header = 0)
+datos = pd.read_csv("poke_mon.csv", header = 0)
 
+id = list(datos["PokeDex_index_number"])
+name = list(datos["Name_of_the_Pokemon"])
+type_1 = list(datos["Type_of_pokemon"])
